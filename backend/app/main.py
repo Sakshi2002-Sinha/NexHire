ba8149581836bf51cc7778fb33a8aa110090d67c
Sakshi Sinha <sakshisinha5291@gmail.com>
2026-06-17 00:38:32 +0530

    update alembic/.env

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,17 +24,23 @@ app=FastAPI()
 
 print(models.Base.metadata.tables.keys())
 models.Base.metadata.create_all(bind=engine)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth_router)
 
 @app.get("/")
